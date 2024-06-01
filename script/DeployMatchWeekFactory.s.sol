8a2132ed1edd63b63e2fc535a9071bab96186df2
Meke <donotmind@fake.com>
2024-06-01 13:45:51 +0200

    Can deploy project with foundry

---
+++ script/DeployMatchWeekFactory.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {MatchWeek} from "../src/MatchWeek.sol";
+import {MatchWeekFactory} from "../src/MatchWeekFactory.sol";
+import {FunctionsConsumer} from "../src/FunctionsConsumer.sol";
+import {MockFunctionsConsumer} from "../test//mock/MockFunctionsConsumer.sol";
+
+contract DeployMatchWeekFactory is Script {
+    function run() external {
+        vm.startBroadcast();
+        FunctionsConsumer consumer = new MockFunctionsConsumer();
+        MatchWeek matchWeek = new MatchWeek();
+
+        MatchWeekFactory factory = new MatchWeekFactory(msg.sender);
+        factory.setLibraryAddress(address(matchWeek));
+        factory.setConsumerAddress(address(consumer));
+        vm.stopBroadcast();
+    }
+}
