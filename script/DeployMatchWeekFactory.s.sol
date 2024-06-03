ba295b5ffec8579bf30a102900aa85097baef8fd
Meke <cryptomeke666@gmail.com>
2024-06-03 18:50:42 +0200

    Added HelperConfig to deploy contracts depending on network

--- script/DeployMatchWeekFactory.s.sol
+++ script/DeployMatchWeekFactory.s.sol
@@ -7,16 +7,18 @@ import {MatchWeek} from "../src/MatchWeek.sol";
 import {MatchWeekFactory} from "../src/MatchWeekFactory.sol";
 import {FunctionsConsumer} from "../src/FunctionsConsumer.sol";
 import {MockFunctionsConsumer} from "../test//mock/MockFunctionsConsumer.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployMatchWeekFactory is Script {
     function run() external {
-        vm.startBroadcast();
-        FunctionsConsumer consumer = new MockFunctionsConsumer();
-        MatchWeek matchWeek = new MatchWeek();
+        HelperConfig helperConfig = new HelperConfig();
+        (address usdtToken, address consumer) = helperConfig.activeNetworkConfig();
 
+        vm.startBroadcast();
+        MatchWeek matchWeek = new MatchWeek();
         MatchWeekFactory factory = new MatchWeekFactory(msg.sender);
         factory.setLibraryAddress(address(matchWeek));
-        factory.setConsumerAddress(address(consumer));
+        factory.setConsumerAddress(consumer);
         vm.stopBroadcast();
     }
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,5 +2,35 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockUsdtToken} from "../test/mock/MockUsdtToken.sol";
+import {MockFunctionsConsumer} from "../test/mock/MockFunctionsConsumer.sol";
 
-contract HelperConfig is Script {}
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address usdtToken;
+        address oracleConsumer;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaNetworkConfig();
+        } else {
+            activeNetworkConfig = getAnvilNetworkConfig();
+        }
+    }
+
+    function getSepoliaNetworkConfig() private view returns (NetworkConfig memory) {}
+
+    function getAnvilNetworkConfig() private returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockUsdtToken usdtToken = new MockUsdtToken();
+        MockFunctionsConsumer consumer = new MockFunctionsConsumer();
+        vm.stopBroadcast();
+
+        NetworkConfig memory networkConfig = NetworkConfig(address(usdtToken), address(consumer));
+
+        return networkConfig;
+    }
+}
